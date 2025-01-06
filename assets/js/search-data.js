75c82c23abe040c300c459f9e20b3a50f7f29ba5
pratajo <prata128@gmail.com>
2025-01-06 23:25:58 +0000

    Deploying to gh-pages from @ pratajo/pratajo.github.io@e07d41e29f2c6ab65349cac6346dd4caad87685c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -431,16 +431,16 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+    },{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-fedora-linux-41-released",
+          title: 'Fedora Linux 41 released!',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
