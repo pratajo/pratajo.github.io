a36f880be2d0fe3a0b426eb5084914b79c4ea8c4
pratajo <prata128@gmail.com>
2025-01-08 21:39:50 +0000

    Deploying to gh-pages from @ pratajo/pratajo.github.io@fe9adb19d6d4e36e9109538160a810a2ca51e2f3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -460,14 +460,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%69%6E%66%6F@%70%72%61%74%61%6A%6F.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-rss',
@@ -477,18 +470,11 @@ ninja.data = [{
           window.open("/feed.xml", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
         id: 'social-custom_social',
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.pratajo.com/en/", "_blank");
         },
       },{
       id: 'light-theme',
