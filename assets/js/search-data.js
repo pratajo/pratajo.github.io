382cb32021e35fcca5e85a897dd7cd585e7b3660
pratajo <prata128@gmail.com>
2025-01-08 22:05:48 +0000

    Deploying to gh-pages from @ pratajo/pratajo.github.io@8f38b6514b843e5060b0413a58cb297fdd0dbb94 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,16 +16,16 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "nav-publications",
-          title: "publications",
-          description: "Author publications.",
+        },{id: "nav-author-publications",
+          title: "Author publications",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
