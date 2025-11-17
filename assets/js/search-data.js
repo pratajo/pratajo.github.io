959a96206abff4235aea39583e9c70c139bcf50f
pratajo <prata128@gmail.com>
2025-11-17 09:44:47 +0000

    Deploying to gh-pages from @ pratajo/pratajo.github.io@4741ae43ef9866834860f6f40b7383d600d5f5ed 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -405,7 +405,7 @@ ninja.data = [{
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.pratajo.com/", "_blank");
+          window.open("https://https://www.pratajo.com/", "_blank");
         },
       },{
       id: 'light-theme',
